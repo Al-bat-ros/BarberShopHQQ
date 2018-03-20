c0945d15cd9ffa27b36b1b71baaa6a0d13a04302
Al-bat-ros <al-bat-ros@mail.ru>
2018-03-20 13:54:41 +0500

    Save client info. the LAME way
    Сохранение в БД ламерский вариант

--- app.rb
+++ app.rb
@@ -20,3 +20,25 @@ end
 get '/visit' do
   erb :visit
 end
+
+
+post '/visit' do
+
+  @barber = params[:barber]
+  @username = params[:user_name]
+  @namber_phone = params[:namber_phone]
+  @data_time = params[:data_time]
+  @color = params[:color]
+
+
+  c = Client.new
+  c.name = @username
+  c.phone = @namber_phone
+  c.datestamp = @data_time
+  c.barber = @barber
+  c.color = @color
+  c.save
+
+
+  erb "<h1>Спасибо, вы записаны</h1>"
+end
